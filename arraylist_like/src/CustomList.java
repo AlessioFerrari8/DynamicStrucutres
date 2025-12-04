a51b73e93591e2c075412751a183071a4cb9b35c
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-12-04 14:24:54 +0100

    Feat: add append function

--- arraylist_like/src/CustomList.java
+++ arraylist_like/src/CustomList.java
@@ -10,4 +10,16 @@ public class CustomList {
     public CustomList(Node newHead) { // starting from a node
         this.head = newHead; // the old head becomes the new
     }
+
+    public void append(Node newNode) {
+        if (head == null) { // se si parte subito con un null pointer
+            head = newNode; // setto come primo il nuovo nodo
+        } else { // altrimenti
+            Node now = head; // metto il nodo attuale come head
+            while (now.getNext() != null) { // controllo fino a quando non trovo il null pointer
+                now = now.getNext(); // setto l'attuale
+            }
+            now.setNext(newNode); // setto il nuovo nodo
+        }
+    }
 }
